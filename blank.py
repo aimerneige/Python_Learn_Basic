0b5df00dcb252ba56c38ed993fd259cd69792e22
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-18 16:20:02 +0800

    difference between left and right

--- blank.py
+++ blank.py
@@ -2,4 +2,6 @@ test_string = 'test'
 test_string_with_blank = 'test '
 print(test_string)
 print(test_string_with_blank)
-print(test_string_with_blank.rstrip())
+print(test_string_with_blank.strip())  # left and right
+print(test_string_with_blank.rstrip()) # right
+print(test_string_with_blank.lstrip()) # left
