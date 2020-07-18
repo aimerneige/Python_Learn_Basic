4e9724e87fde1d3824e49a8497849aa4a9188660
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-19 00:18:56 +0800

    range

---
+++ range.py
@@ -0,0 +1,11 @@
+numbers = list(range(1, 6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+squares = []
+for i in range(1, 11):
+    square = i**2
+    squares.append(square)
+print(squares)
