7866fdf85a9b1cbf5f82ea0feb434cd84f4381a6
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-19 00:46:21 +0800

    4-6

---
+++ practice_4-6.py
@@ -0,0 +1,9 @@
+odd_numbers = []
+for i in range(1, 20, 2):
+    odd_numbers.append(i)
+for num in odd_numbers:
+    print(num)
+
+print(odd_numbers)
+odds = [i for i in range(1, 20, 2)]
+print(odds)
