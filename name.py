f2a421d477dfa32c5ef4c18fbe20618289424b97
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-18 16:12:22 +0800

    + with string

--- name.py
+++ name.py
@@ -1,2 +1,9 @@
 name = 'aimer neige'
 print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = 'Aimer'
+second_name = 'Neige'
+full_name = first_name + ' ' + second_name
+print(full_name)
