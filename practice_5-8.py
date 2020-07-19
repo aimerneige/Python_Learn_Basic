5f73845f849adcdc47d9ded58018b35d6d9c5ea4
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-19 13:29:16 +0800

    5-8

---
+++ practice_5-8.py
@@ -0,0 +1,10 @@
+user_list = ['admin', 'aimerneige', 'monika', 'sayori', 'natsuki']
+
+if user_list:
+    for usr in user_list:
+        if usr == 'admin':
+            print("admin!")
+        else:
+            print("user!")
+else:
+    print('empty!')
