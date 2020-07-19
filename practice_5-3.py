8574e4608191338473945687434be9da79c7f0eb
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-19 12:36:36 +0800

    5-3

---
+++ practice_5-3.py
@@ -0,0 +1,21 @@
+# 5-3/4/5
+
+alien_color = ['green', 'red', 'yellow']
+
+for alien in alien_color:
+    if alien == 'green':
+        print("You got 5 coins!")
+
+for alien in alien_color:
+    if alien == 'green':
+        print("You got 5 coins!")
+    else:
+        print("You got nothing!")
+
+for alien in alien_color:
+    if alien == 'green':
+        print("You got 5 coins!")
+    elif alien == 'yellow':
+        print("You got 10 coins!")
+    elif alien == 'red':
+        print("You got 15 coins!")
