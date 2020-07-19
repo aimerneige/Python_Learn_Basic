d0fc896c7d5967000509351f4883638f9e91bffe
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-19 12:06:53 +0800

    not

---
+++ notin.py
@@ -0,0 +1,10 @@
+banned = ['andrew', 'carolina', 'david']
+user = 'Aimer'
+
+if user not in banned:
+    print("You have the permission")
+
+if not (1 != 3):
+    print('Hello')
+else:
+    print("Hi")
