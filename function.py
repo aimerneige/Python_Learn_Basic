e904fc9d8ac05e7fa03cf9a88308fd609b271c76
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-20 15:33:32 +0800

    function

--- function.py
+++ function.py
@@ -11,3 +11,11 @@ If you use the keyword to describe the parm
 you can ignore the order of the parm but you
 must make sure the parm name is right
 """
+
+# default value
+
+def describe_pet_1(pet_name, animal_type='dog'):
+    print('type: ', animal_type)
+    print('name: ', pet_name)
+
+describe_pet_1('willie')
