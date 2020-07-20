23338bf74f7d7139d0043d3909305bb121736ec3
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-20 12:19:45 +0800

    function

---
+++ function.py
@@ -0,0 +1,13 @@
+def describe_pet(animal_type, pet_name):
+    print('type: ', animal_type)
+    print('name: ', pet_name)
+
+describe_pet("dog", "Jack")
+
+describe_pet(pet_name='harry', animal_type='cat')
+
+"""
+If you use the keyword to describe the parm
+you can ignore the order of the parm but you
+must make sure the parm name is right
+"""
