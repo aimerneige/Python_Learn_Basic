d262ad7491a1aad57e352110c3c804a06b3abc66
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-20 11:18:06 +0800

    greeter

---
+++ greeter.py
@@ -0,0 +1,10 @@
+def greet_user():
+    print("Hello!")
+
+greet_user()
+
+# with user
+def greet_user_2(user_name):
+    print("Hello, " + user_name + ". Nice to meet you.")
+
+greet_user_2("Aimer Neige")
