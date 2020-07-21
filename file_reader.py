7fa7ac4428cd3a9bd3cf866b0a516dc1adbd4b52
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-22 00:32:33 +0800

    for line

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,11 @@
 with open('pi_digits.txt') as file_obj:
     contens = file_obj.read()
-    print(contens)
+    print(contens.rstrip())
+
+with open('pi_digits.txt') as file_obj:
+    for line in file_obj:
+        print(line.rstrip())
+
+with open('pi_digits.txt') as file_obj:
+    for sss in file_obj:
+        print(sss.rstrip())
