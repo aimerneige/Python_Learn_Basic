576665c07f421de34c94d9beda6fd89d3b01bd9b
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-21 23:59:01 +0800

    fl

---
+++ favorite_language.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+
+favorite_languages = OrderedDict()
+
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
+
+for name, languages in favorite_languages.items():
+    print(name.title(), languages)
