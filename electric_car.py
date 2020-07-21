9cda609619dd6a4684e4ee7dea1143be2c1ed7ce
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-21 23:45:28 +0800

    e

---
+++ electric_car.py
@@ -0,0 +1,38 @@
+class Car():
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print(self.odometer_reading)
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("you can't roll back")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+test = ElectricCar('tesla', 'model s', 2016)
+print(test.get_descriptive_name())
+test.describe_battery()
