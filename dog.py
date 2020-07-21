5fbef805e7680305db5cda5c7424f19f2ff53397
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-21 14:33:31 +0800

    Dog

---
+++ dog.py
@@ -0,0 +1,15 @@
+class Dog():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        print(self.name.title() + " rolled over!")
+
+my_dog = Dog("jack", 4)
+my_dog.sit()
+my_dog.roll_over()
